b9382e02c93a214cb6f6c16cb5fa3100dc55d2fc
alpernai <148302974+alpernai@users.noreply.github.com>
2024-06-17 21:14:31 +0000

    excuseGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateRandomExcuse() {
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(
+    what
+  )} ${getRandom(when)}`;
+  return excuse;
+}
+
+function setExcuse() {
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = generateRandomExcuse();
+}
+
+window.onload = setExcuse;
